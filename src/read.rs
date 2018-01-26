9ab08c44a006e2e0ba88aed7a3ff03d343be42ba
Itai Bar-Natan <itaibn3141@gmail.com>
2018-01-25 21:50:51 -0500

    Whitespace support; iterator

--- src/read.rs
+++ src/read.rs
@@ -3,12 +3,15 @@ use std::collections::HashMap;
 
 use regex::{Captures, Regex};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     LeftParen,
     RightParen,
     Identifier(String),
 }
+
+#[derive(Debug)]
+pub struct Lexer<'a>(&'a str);
 
 macro_rules! grammar {
     ($($key:ident -> $value:expr;)*) => {lazy_static! {
@@ -22,25 +25,29 @@ macro_rules! grammar {
     }}
 }
 
+// No unicode support
 grammar! {
+    capture_token -> "^<<intertoken_space>><<token>>";
+
     // Incomplete
     delimiter -> r"(:?[|() \t\n\r]|$)";
-
+    // Incomplete
+    token -> r"(?P<token><<identifier>>|\(|\))";
+    // Incomplete
+    intraspace_whitespace -> r"[ \t]";
+    // Incomplete
+    whitespace -> r"<<intraspace_whitespace>>";
+    // Incomplete
+    atmosphere -> r"<<whitespace>>";
+    intertoken_space -> r"<<atmosphere>>*";
     // Incomplete
     identifier -> r"<<initial>><<subsequent>>*";
-
     initial -> r"(:?<<letter>>|<<special_initial>>)";
-
     letter -> r"[a-zA-Z]";
-
     special_initial -> r"[!$%&*:/<=>?@^_~]";
-
     subsequent -> r"(:?<<initial>>|<<digit>>|<<special_subsequent>>)";
-
     digit -> r"[0-9]";
-
     explicit_sign -> r"[+-]";
-
     special_subsequent -> r"(:?<<explicit_sign>>|[.@])";
 }
 
@@ -86,6 +93,41 @@ fn regex_class(class: &'static str) -> Regex {
     Regex::new(REGEX_STRINGS.get(class).unwrap()).unwrap()
 }
 
+fn read_token(input: &str) -> Option<(Token, &str)> {
+    let captures = match regex_class("capture_token").captures(input) {
+        None => return None,
+        Some(x) => x,
+    };
+
+    let m = captures.name("token").unwrap();
+    let rest = &input[m.end()..];
+
+    let token = match m.as_str() {
+        "(" => Token::LeftParen,
+        ")" => Token::RightParen,
+        ident => Token::Identifier(ident.to_string()),
+    };
+
+    Some((token, rest))
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &str) -> Lexer {
+        Lexer(input)
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Result<Token, &'static str>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        read_token(self.0).map(|(token, rest)| {
+            self.0 = rest;
+            Ok(token)
+        })
+    }
+}
+
 #[test]
 pub fn test_regex_strings_gen() {
     ::lazy_static::initialize(&REGEX_STRINGS);
@@ -121,4 +163,32 @@ fn test_identifier_2() {
     let re = regex_class("identifier");
     let m = re.find(".+-09A@3 x").unwrap();
     assert_eq!((m.start(), m.end()), (5, 8));
+}
+
+#[test]
+fn test_token_0() {
+    assert_eq!(read_token("("), Some((Token::LeftParen, "")));
+}
+
+#[test]
+fn test_token_1() {
+    assert_eq!(read_token(" )"), Some((Token::RightParen, "")));
+}
+
+#[test]
+fn test_token_2() {
+    assert_eq!(read_token("blah blub"), Some((Token::Identifier(
+        "blah".to_string()), " blub")));
+}
+
+#[test]
+fn test_token_3() {
+    assert_eq!(read_token("0.. ("), None);
+}
+
+#[test]
+fn test_lexer() {
+    assert_eq!(Lexer::new("(x y)").collect::<Result<Vec<_>, _>>(),
+        Ok(vec![Token::LeftParen, Token::Identifier("x".to_string()),
+        Token::Identifier("y".to_string()), Token::RightParen]));
 }
