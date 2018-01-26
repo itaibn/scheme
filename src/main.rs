8a2ffbd273831fb82422d3aa2e4401aba5a0b3d2
Itai Bar-Natan <itaibn3141@gmail.com>
2018-01-25 22:30:49 -0500

    Add simple parsing into expressions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,17 @@ extern crate regex;
 
 mod read;
 
+#[derive(Debug)]
+pub enum Scheme {
+    Symbol(String),
+    Nil,
+    Cons(Box<Scheme>, Box<Scheme>),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input_buffer = String::new();
+    std::io::stdin().read_line(&mut input_buffer).expect("IO error reading \
+        input");
+    println!("{:?}", read::Reader::new(&input_buffer).read_expr().expect(
+        "Invalid expression"));
 }
---
+++ src/read/mod.rs
@@ -0,0 +1,73 @@
+mod lexer;
+
+use ::Scheme;
+use self::lexer::{Lexer, Token};
+
+use std::mem;
+
+pub struct Reader<'a> {
+    // Fuse?
+    lexer: Lexer<'a>,
+    cur_token: Option<Token>,
+}
+
+impl<'a> Reader<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Reader {
+            lexer: Lexer::new(input),
+            cur_token: None,
+        }
+    }
+
+    fn fill_cur_token(&mut self) -> Result<(), &'static str> {
+        if self.cur_token.is_some() {
+            return Ok(());
+        }
+
+        match self.lexer.next() {
+            Some(Ok(tok)) => self.cur_token = Some(tok),
+            Some(Err(err)) => return Err(err),
+            None => {},
+        }
+
+        Ok(())
+    }
+
+    pub fn peek_token(&mut self) -> Result<Option<&Token>, &'static str> {
+        self.fill_cur_token()?;
+
+        Ok(self.cur_token.as_ref())
+    }
+
+    pub fn read_token(&mut self) -> Result<Option<Token>, &'static str> {
+        self.fill_cur_token()?;
+        Ok(mem::replace(&mut self.cur_token, None))
+    }
+
+    pub fn read_expr(&mut self) -> Result<Scheme, &'static str> {
+        match self.read_token()? {
+            Some(Token::Identifier(ident)) => {
+                Ok(Scheme::Symbol(ident))
+            },
+            Some(Token::LeftParen) => self.read_list(),
+            _ => Err("Invalid expression"),
+        }
+    }
+
+    fn read_list(&mut self) -> Result<Scheme, &'static str> {
+        let mut list = Vec::new();
+
+        while self.peek_token()? != Some(&Token::RightParen) {
+            list.push(self.read_expr()?);
+        }
+
+        assert_eq!(self.read_token(), Ok(Some(Token::RightParen)));
+
+        let mut list_expr = Scheme::Nil;
+        for term in list.into_iter().rev() {
+            list_expr = Scheme::Cons(Box::new(term), Box::new(list_expr));
+        }
+
+        Ok(list_expr)
+    }
+}
