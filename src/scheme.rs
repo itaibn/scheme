22075b863513608b3eb07671c83cb01a1cb7f48c
Itai Bar-Natan <itaibn3141@gmail.com>
2018-02-11 15:32:57 -0500

    Implement as_* methods for Scheme

--- src/scheme.rs
+++ src/scheme.rs
@@ -2,7 +2,7 @@
 use std::collections::HashMap;
 use std::iter::DoubleEndedIterator;
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum SchemeData {
     Null,
     Cons(Box<Scheme>, Box<Scheme>),
@@ -16,71 +16,6 @@ pub type Scheme = SchemeData;
 type Builtin = fn(Vec<Scheme>) -> Scheme;
 
 pub struct Environment(HashMap<String, Scheme>);
-
-impl SchemeData {
-    // Use iterators
-    fn into_vec(mut self) -> Option<Vec<Scheme>> {
-        let mut cur_elems = Vec::new();
-
-        loop {
-            match self {
-                SchemeData::Cons(car, cdr) => {
-                    cur_elems.push(*car);
-                    self = *cdr;
-                },
-                SchemeData::Null => {
-                    return Some(cur_elems);
-                },
-                _ => {
-                    return None;
-                },
-            }
-        }
-    }
-
-    fn list_from_iter<I: IntoIterator<Item=Scheme>>(iter: I) -> Scheme where
-        I::IntoIter : DoubleEndedIterator {
-        
-        let mut res = Scheme::null();
-        for elem in iter.into_iter().rev() {
-            res = Scheme::cons(elem, res);
-        }
-        res
-    }
-
-    // Incorporate errors to type signature
-    pub fn eval(self) -> Scheme {
-        match self {
-            SchemeData::Symbol(s) => {
-                if &s == "sum" {
-                    Scheme::builtin(sum_builtin)
-                } else {
-                    Scheme::symbol(s.clone())
-                }
-            }
-            itself @ SchemeData::Cons(_, _) => {
-                let args = itself.into_vec();
-                // TODO: Special forms
-                let mut exprs = args.unwrap()
-                                    .into_iter()
-                                    .map(|arg| arg.eval())
-                                    .collect::<Vec<_>>();
-                let args = exprs.split_off(1);
-                exprs.into_iter().next().unwrap().apply(args)
-            },
-            // Temp
-            _ => self,
-        }
-    }
-
-    fn apply(self, args: Vec<Scheme>) -> Scheme {
-        if let SchemeData::Builtin(builtin) = self {
-            builtin(args)
-        } else {
-            Scheme::cons(self, SchemeData::list_from_iter(args))
-        }
-    }
-}
 
 impl Scheme {
     pub fn null() -> Scheme {
@@ -138,14 +73,71 @@ impl Scheme {
             None
         }
     }
+
+    // Use iterators
+    fn into_vec(&self) -> Option<Vec<Scheme>> {
+        let mut cur_elems = Vec::new();
+        let mut head = self;
+
+        loop {
+            if let Some((car, cdr)) = head.as_pair() {
+                cur_elems.push(car.clone());
+                head = cdr;
+            } else if head.is_null() {
+                return Some(cur_elems);
+            } else {
+                return None;
+            }
+        }
+    }
+
+    fn list_from_iter<I: IntoIterator<Item=Scheme>>(iter: I) -> Scheme where
+        I::IntoIter : DoubleEndedIterator {
+        
+        let mut res = Scheme::null();
+        for elem in iter.into_iter().rev() {
+            res = Scheme::cons(elem, res);
+        }
+        res
+    }
+
+    // Incorporate errors to type signature
+    pub fn eval(&self) -> Scheme {
+        if let Some(s) = self.as_symbol() {
+            if s == "sum" {
+                Scheme::builtin(sum_builtin)
+            } else {
+                Scheme::symbol(s.to_string())
+            }
+        } else if self.as_pair().is_some() {
+            let args = self.into_vec();
+            // TODO: Special forms
+            let mut exprs = args.unwrap()
+                                .into_iter()
+                                .map(|arg| arg.eval())
+                                .collect::<Vec<_>>();
+            let args = exprs.split_off(1);
+            exprs.into_iter().nth(0).unwrap().apply(args)
+        } else {
+            // Temp
+            self.clone()
+        }
+    }
+
+    fn apply(&self, args: Vec<Scheme>) -> Scheme {
+        if let Some(builtin) = self.as_builtin() {
+            builtin(args)
+        } else {
+            Scheme::cons(self.clone(), SchemeData::list_from_iter(args))
+        }
+    }
 }
 
 fn sum_builtin(args: Vec<Scheme>) -> Scheme {
     let mut total = 0;
     for arg in args {
-        match arg {
-            SchemeData::Int(n) => total += n,
-            _ => {},
+        if let Some(n) = arg.as_int() {
+            total += n;
         }
     }
     Scheme::int(total)
